c623c26e0c148a11088fa207d52409026b65cae6
Yash Patodkar <yashpatodkar@Yashs-MacBook-Pro-8.local>
2024-12-05 14:18:01 -0800

    Logger file

---
+++ Project/src/main/java/com/project/exceptions/WaterOverflowException.java
@@ -0,0 +1,7 @@
+package com.project.exceptions;
+
+public class WaterOverflowException extends RuntimeException {
+    public WaterOverflowException(String message) {
+        super(message);
+    }
+}
--- Project/src/main/java/com/project/logger/Logger.java
+++ Project/src/main/java/com/project/logger/Logger.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Logger {
-    private static final String LOG_FILE = "garden_log.txt"; // Log file name
+    private static final String LOG_FILE = "com/project/logger/garden_log.txt"; // Log file name
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     // Log levels
